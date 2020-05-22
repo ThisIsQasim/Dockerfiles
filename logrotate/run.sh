da16fa5b0c66e600b05570cdc9a820b63712622b
Qasim Mehmood <qasim12@outlook.com>
2020-05-23 01:09:33 +0500

    listen for kill signal

--- logrotate/run.sh
+++ logrotate/run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+kill_children()
+{
+    kill -SIGINT $(jobs -p)
+    exit #$
+}
+trap kill_children SIGINT
+
 while true; do
-    sleep $(( $INTERVAL - ( $INTERVAL * $JITTER / 100 ) + ( $RANDOM % ( $INTERVAL * 2 * $JITTER / 100 ) ) ))
+    sleep $(( $INTERVAL - ( $INTERVAL * $JITTER / 100 ) + ( $RANDOM % ( $INTERVAL * 2 * $JITTER / 100 ) ) )) &
+    wait
     /usr/sbin/logrotate -v -s $STATUSFILE /etc/logrotate.conf
 done
